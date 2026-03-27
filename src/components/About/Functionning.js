f20cb06f3335f9bdae6d276f19eb61b1595473c9
Aman Desire <desireaman000@gmail.com>
2026-03-27 17:52:25 +0000

    Structure modification in Functionning.js

--- src/components/About/Functionning.js
+++ src/components/About/Functionning.js
@@ -6,7 +6,7 @@ import regiment from "@/public/img/regiment.png"
 
 export default function FunctioningSection() {
     return (
-        <section id="organization" className="py-20 flex flex-col">
+        <section id="organization" className="py-20 flex flex-col bg-orange-50 bg-opacity-10">
             <div className="container mx-auto px-6">
                 {/* Titre */}
                 <div className="text-center mb-12">
@@ -28,26 +28,30 @@ export default function FunctioningSection() {
                     <Image alt="Organigramme ANM" className="w-[1280px] h-auto mx-auto" src={organigramme} />
                 </div>
                 {/* Les régiments */}
-                <div className="mb-16 flex flex-row">
+                <div className="mb-16 flex flex-col md:flex-row">
                     <div className="flex flex-col my-auto">
                         <h3 className="text-xl font-semibold text-orange-800 mb-4">Les régiments</h3>
-                        <p className="text-gray-700 mb-4">
-                            L’ANM est composée de plusieurs régiments, qui constituent les unités de base de l’association.
-                            Chaque régiment regroupe des membres qui :
-                        </p>
+                        {/* Texte */}
+                        <div className="flex flex-col">
+                            <p className="text-gray-700 mb-4">
+                                L’ANM est composée de plusieurs régiments, qui constituent les unités de base de l’association.
+                                Chaque régiment regroupe des membres qui :
+                            </p>
 
-                        {/* Liste */}
-                        <ul className="text-gray-700 list-disc list-inside mb-8 inline-block text-left">
-                            <li>participent aux activités de terrain,</li>
-                            <li>contribuent aux projets, et</li>
-                            <li>représentent l’ANM dans leurs zones ou domaines.</li>
-                        </ul>
-                        <p className="mt-8 text-gray-700 italic">
-                            <strong>N.B. :</strong>Tous les membres de la Direction (Président, Secrétaire Général, etc.) sont également membres d’un régiment.
-                        </p>
+                            {/* Liste */}
+                            <ul className="text-gray-700 list-disc list-inside mb-8 inline-block text-left">
+                                <li>participent aux activités de terrain,</li>
+                                <li>contribuent aux projets, et</li>
+                                <li>représentent l’ANM dans leurs zones ou domaines.</li>
+                            </ul>
+                            <p className="mt-8 text-gray-700 italic">
+                                <strong>N.B. :</strong>Tous les membres de la Direction (Président, Secrétaire Général, etc.) sont également membres d’un régiment.
+                            </p>
+                        </div>
+
                     </div>
                     {/* Cercle des régiments */}
-                    <div>
+                    <div className="mt-16 mx-auto">
                         <Image
                             alt="Fonctionnement des Regiments"
                             className="w-[400px] h-auto mx-auto"
